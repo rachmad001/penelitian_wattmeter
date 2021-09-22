c1418c03aaa9b120b9c5f56b316c01ef1e6075e6
rachmad001 <60819206+rachmad001@users.noreply.github.com>
2021-09-22 10:10:30 +0800

    updated server

--- server/config.php
+++ server/config.php
@@ -1,8 +1,8 @@
 <?php
-    $server = "localhost:3307";
-    $username = "root";
-    $password = "";
-    $database = "penelitian";
+    $server = "localhost:3306";
+    $username = "peneliti_sukri";
+    $password = "@QazXsw21";
+    $database = "peneliti_wattMeter";
 
     $mysqli = mysqli_connect($server, $username, $password, $database);
 ?>
--- server/data.php
+++ server/data.php
@@ -3,12 +3,15 @@
 	include "config.php";
 
 	$result = mysqli_query($mysqli, "select * from sensor");
-	$hasil = mysqli_fetch_assoc($result);
-	$myObj = array(
-		"volt" => $hasil["volt"],
-		"ampere" => $hasil["ampere"],
-		"watt" => $hasil["watt"]
-	);
+	$start = 0;
+	while ($hasil = mysqli_fetch_assoc($result)){
+    	$myObj[$start] = array(
+    		"volt" => $hasil["volt"],
+    		"ampere" => $hasil["ampere"],
+    		"watt" => $hasil["watt"]
+    	);
+    	$start++;
+	}
 	$myJson = json_encode($myObj);
 	echo $myJson;
 ?>
